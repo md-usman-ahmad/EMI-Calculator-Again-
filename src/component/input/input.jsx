fc4252d21e15f3e05f08ed7ce461c0f101b07533
Usmaan <Usmaan@usmaan.com>
2025-09-10 02:46:46 +0530

    Without Calculate Button

--- src/component/input/input.jsx
+++ src/component/input/input.jsx
@@ -1,50 +1,126 @@
-
-                                              // With Calculate button  
-
 import { useState } from "react";
-export function Input({EMICalculation}) {
-    const [userInputs,setUserInputs] = useState({
-        loanAmount:undefined,
-        rateOfInterest: undefined,
-        loanTenure: undefined,
-    })
-    console.log("userInput - >",userInputs);
+
+// Without Calculate Button
+export function Input({ EMICalculation }) {
+  const [userInputs, setUserInputs] = useState({
+    loanAmount: 100000,
+    rateOfInterest: 1,
+    loanTenure: 1,
+  });
+  console.log("userInput - >", userInputs);
   return (
     <>
-        <div className="mb-3">
-          <label className="form-label">Loan Amount(₹)</label>
-          <input
-            type="text"
-            className="form-control me-2"
-            id="name"
-            placeholder="Enter Amount"
-            aria-label="Search"
-            onChange={ (event)=>{setUserInputs( {...userInputs,loanAmount: event.target.value} )} }
-          />
+      <div className="mb-3">
+        <div className=" d-flex justify-content-between ">
+            <label className="form-label">Loan Amount(₹)</label>
+            <h6>[Minimum 1Lakh]</h6>
         </div>
-        <div className="mb-3">
-          <label className="form-label">Rate of Interest(%)</label>
-          <input
-            type="text"
-            className="form-control me-2"
-            id="name"
-            placeholder="Enter Percentage"
-            aria-label="Search"
-            onChange={ (event)=>{setUserInputs( {...userInputs,rateOfInterest: event.target.value} )} }
-          />
+        <input
+          type="text"
+          className="form-control me-2"
+          id="name"
+          placeholder="Enter Amount"
+          aria-label="Search"
+          defaultValue={10000}
+          onChange={(event) => {
+            const newInputs = { ...userInputs, loanAmount: event.target.value };
+            setUserInputs(newInputs);
+            EMICalculation(newInputs);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <div className=" d-flex justify-content-between ">
+            <label className="form-label">Rate of Interest(%)</label>
+            <h6>[1-30]</h6>
         </div>
-        <div className="mb-3">
-          <label className="form-label">Loan Tenure(Yr)</label>
-          <input
-            type="text"
-            className="form-control me-2"
-            id="name"
-            placeholder="Enter No of Years"
-            aria-label="Search"
-            onChange={ (event)=>{setUserInputs( {...userInputs,loanTenure: event.target.value} )} }
-          />
+        <input
+          type="text"
+          className="form-control me-2"
+          id="name"
+          placeholder="Enter Percentage"
+          aria-label="Search"
+          defaultValue={1}
+          onChange={(event) => {
+            const newInputs = {
+              ...userInputs,
+              rateOfInterest: event.target.value
+            }
+            setUserInputs(newInputs);
+            EMICalculation(newInputs);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <div className=" d-flex justify-content-between ">
+            <label className="form-label">Loan Tenure(Yr)</label>
+            <h6>[1-30]</h6>
         </div>
-        <button type="button" className="btn btn-outline-primary" onClick={ ()=>{EMICalculation(userInputs)} }>Calculate</button>
+        <input
+          type="text"
+          className="form-control me-2"
+          id="name"
+          placeholder="Enter No of Years"
+          aria-label="Search"
+          defaultValue={1 }
+          onChange={(event) => {
+            const newInputs = {
+              ...userInputs,
+              loanTenure: event.target.value
+            }
+            setUserInputs(newInputs);
+            EMICalculation(newInputs);
+          }}
+        />
+      </div>
     </>
   );
 }
+
+// With Calculate button
+// export function Input({EMICalculation}) {
+//     const [userInputs,setUserInputs] = useState({
+//         loanAmount:undefined,
+//         rateOfInterest: undefined,
+//         loanTenure: undefined,
+//     })
+//     console.log("userInput - >",userInputs);
+//   return (
+//     <>
+//         <div className="mb-3">
+//           <label className="form-label">Loan Amount(₹)</label>
+//           <input
+//             type="text"
+//             className="form-control me-2"
+//             id="name"
+//             placeholder="Enter Amount"
+//             aria-label="Search"
+//             onChange={ (event)=>{setUserInputs( {...userInputs,loanAmount: event.target.value} )} }
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label className="form-label">Rate of Interest(%)</label>
+//           <input
+//             type="text"
+//             className="form-control me-2"
+//             id="name"
+//             placeholder="Enter Percentage"
+//             aria-label="Search"
+//             onChange={ (event)=>{setUserInputs( {...userInputs,rateOfInterest: event.target.value} )} }
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label className="form-label">Loan Tenure(Yr)</label>
+//           <input
+//             type="text"
+//             className="form-control me-2"
+//             id="name"
+//             placeholder="Enter No of Years"
+//             aria-label="Search"
+//             onChange={ (event)=>{setUserInputs( {...userInputs,loanTenure: event.target.value} )} }
+//           />
+//         </div>
+//         <button type="button" className="btn btn-outline-primary" onClick={ ()=>{EMICalculation(userInputs)} }>Calculate</button>
+//     </>
+//   );
+// }
