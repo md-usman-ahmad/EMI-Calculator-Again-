f7d312293b7d38fe42aef44fe296414bedcab833
Usmaan <Usmaan@usmaan.com>
2025-09-10 06:28:25 +0530

    With Calculate Button(2.0), just another way

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,160 @@
 import { Input } from "./component/input/input.jsx";
 import { useState, useRef } from "react";
 
+// Without Calculate Button
+// export function App() {
+//     const [EMI_Values,setEMI_Values] = useState({
+//         emi: undefined,
+//         principalAmount: undefined,
+//         totalAmount: undefined,
+//         totalInterest: undefined
+//     })
 
-                                  // Without Calculate Button 
+//     if(EMI_Values.emi === undefined || EMI_Values.principalAmount === undefined || EMI_Values.totalAmount === undefined || EMI_Values.emi === undefined ){
+//         let p = 100000;
+//         let r = 1/(12*100);
+//         let n = 1*12;
+//         setEMI_Values({
+//             emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
+//             principalAmount: p,
+//             totalAmount: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n,
+//             totalInterest: (Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n) - p
+//         })
+//     }
+
+//     function EMICalculation(userInputs){
+//         console.log("App.jsx me pass hua userInputs = ",userInputs);
+
+//        if( (userInputs.loanAmount > 0 && userInputs.rateOfInterest > 0 && userInputs.loanTenure > 0) ){
+//         if(userInputs.loanAmount < 100000 ){
+//           userInputs.loanAmount = 100000
+//         }
+//         if(userInputs.rateOfInterest < 1 ){
+//           userInputs.rateOfInterest = 1
+//         }
+//         if(userInputs.rateOfInterest > 30 ){
+//           userInputs.rateOfInterest = 30
+//         }
+//         if(userInputs.loanTenure < 1 ){
+//           userInputs.loanTenure = 1
+//         }
+//         if(userInputs.loanTenure > 30 ){
+//           userInputs.loanTenure = 30
+//         }
+
+//           console.log("loanAmount = ",userInputs.loanAmount , "rateOfInterest = " ,userInputs.rateOfInterest , "loanTenure = " ,userInputs.loanTenure)
+
+//           let p = userInputs.loanAmount;
+//           let r = userInputs.rateOfInterest/(12*100);
+//           let n = userInputs.loanTenure*12;
+
+//           setEMI_Values({
+//               emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
+//               principalAmount: p,
+//               totalAmount: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n,
+//               totalInterest: (Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n) - p
+//           })
+//        }
+//     }
+
+//   return (
+//     <>
+//       <p className=" fw-bold display-6 text-center pt-2">EMI Calculator</p>
+//       <div className="container ">
+//         <div className="row pt-4 justify-content-center">
+//           <div
+//             className="col-sm-4 me-3 p-4 border border-end-0 border-2  rounded rounded-5 shadow"
+//             style={{ boxShadow: "-5px 5px 5px rgb(199, 198, 198) !important" }}
+//           >
+//             <Input EMICalculation={ EMICalculation } ></Input>
+
+//             <div className="mt-4 border rounded shadow-lg p-3">
+//                 <div className=" d-flex justify-content-between ">
+//                     <h5>Monthly EMI</h5>
+//                     <p>₹{EMI_Values.emi}</p>
+//                 </div>
+//                 <div className=" d-flex justify-content-between ">
+//                     <h5>Principal Amount</h5>
+//                     <p>₹{EMI_Values.principalAmount}</p>
+//                 </div>
+//                 <div className=" d-flex justify-content-between ">
+//                     <h5>Total Interest</h5>
+//                     <p>₹{EMI_Values.totalInterest}</p>
+//                 </div>
+//                 <div className=" d-flex justify-content-between ">
+//                     <h5>Total Amount</h5>
+//                     <p>₹{EMI_Values.totalAmount}</p>
+//                 </div>
+//             </div>
+
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// With Calculate button (2.0)
 export function App() {
-    const [EMI_Values,setEMI_Values] = useState({
-        emi: undefined,
-        principalAmount: undefined,
-        totalAmount: undefined,
-        totalInterest: undefined
+  const [userInputs, setUserInputs] = useState({
+    loanAmount: 100000,
+    rateOfInterest: 1,
+    loanTenure: 1,
+  });
+  console.log(userInputs);
+
+  const [EMI_Values,setEMI_Values] = useState({
+    emi: undefined,
+    principalAmount: undefined,
+    totalAmount: undefined,
+    totalInterest: undefined
+  })
+
+  if(EMI_Values.emi === undefined || EMI_Values.principalAmount === undefined || EMI_Values.totalAmount === undefined || EMI_Values.emi === undefined ){
+    let p = 100000;
+    let r = 1/(12*100);
+    let n = 1*12;
+    setEMI_Values({
+      emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
+      principalAmount: p,
+      totalAmount: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n,
+      totalInterest: (Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n) - p
     })
+  }
 
-    if(EMI_Values.emi === undefined || EMI_Values.principalAmount === undefined || EMI_Values.totalAmount === undefined || EMI_Values.emi === undefined ){
-        let p = 100000;
-        let r = 1/(12*100);
-        let n = 1*12;
-        setEMI_Values({
-            emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
-            principalAmount: p,
-            totalAmount: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n,
-            totalInterest: (Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n) - p
-        })
+  
+
+  function EMICalculation() {
+    console.log("onClick userInputs = ",userInputs);
+    if ( !(userInputs.loanAmount > 0 && userInputs.rateOfInterest > 0 && userInputs.loanTenure > 0)) {
+      alert("inputs Should be greater than 0");
+      return;
+    }
+    if (!(userInputs.loanAmount >= 100000)) {
+      alert("loan Amount should start from 1lakh");
+      return;
+    }
+    if ((userInputs.rateOfInterest > 30)) {
+      alert("rate of Interest should b/w [1-30]");
+      return;
+    }
+    if ((userInputs.loanTenure > 30)) {
+      alert("loanTenure should b/w [1-30]");
+      return;
     }
 
-    function EMICalculation(userInputs){
-        console.log("App.jsx me pass hua userInputs = ",userInputs);
+    let p = userInputs.loanAmount;
+    let r = userInputs.rateOfInterest / (12 * 100);
+    let n = userInputs.loanTenure * 12;
 
-       if( (userInputs.loanAmount > 0 && userInputs.rateOfInterest > 0 && userInputs.loanTenure > 0) ){
-        if(userInputs.loanAmount < 100000 ){
-          userInputs.loanAmount = 100000
-        }
-        if(userInputs.rateOfInterest < 1 ){
-          userInputs.rateOfInterest = 1
-        }
-        if(userInputs.rateOfInterest > 30 ){
-          userInputs.rateOfInterest = 30
-        }
-        if(userInputs.loanTenure < 1 ){
-          userInputs.loanTenure = 1
-        }
-        if(userInputs.loanTenure > 30 ){
-          userInputs.loanTenure = 30
-        }
-
-          console.log("loanAmount = ",userInputs.loanAmount , "rateOfInterest = " ,userInputs.rateOfInterest , "loanTenure = " ,userInputs.loanTenure)
-          let p = userInputs.loanAmount;
-          let r = userInputs.rateOfInterest/(12*100);
-          let n = userInputs.loanTenure*12;
-
-          setEMI_Values({
-              emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
-              principalAmount: p,
-              totalAmount: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n,
-              totalInterest: (Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n) - p
-          })
-       }  
-    }
+    setEMI_Values({
+      emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
+      principalAmount: p,
+      totalAmount: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n,
+      totalInterest: (Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ) * n) - p
+    })
+  }
+  
 
   return (
     <>
@@ -66,25 +165,31 @@ export function App() {
             className="col-sm-4 me-3 p-4 border border-end-0 border-2  rounded rounded-5 shadow"
             style={{ boxShadow: "-5px 5px 5px rgb(199, 198, 198) !important" }}
           >
-            <Input EMICalculation={ EMICalculation } ></Input>
+            <Input
+              EMICalculation={EMICalculation}
+              {...userInputs}
+              updatedInputs={(property, currentValue) => {
+                setUserInputs({ ...userInputs, [property]: currentValue });
+              }}
+            ></Input>
 
             <div className="mt-4 border rounded shadow-lg p-3">
-                <div className=" d-flex justify-content-between ">
-                    <h5>Monthly EMI</h5>
-                    <p>₹{EMI_Values.emi}</p>
-                </div>
-                <div className=" d-flex justify-content-between ">
-                    <h5>Principal Amount</h5>
-                    <p>₹{EMI_Values.principalAmount}</p>
-                </div>
-                <div className=" d-flex justify-content-between ">
-                    <h5>Total Interest</h5>
-                    <p>₹{EMI_Values.totalInterest}</p>
-                </div>
-                <div className=" d-flex justify-content-between ">
-                    <h5>Total Amount</h5>
-                    <p>₹{EMI_Values.totalAmount}</p>
-                </div>
+              <div className=" d-flex justify-content-between ">
+                <h5>Monthly EMI</h5>
+                <p>₹{EMI_Values.emi}</p>
+              </div>
+              <div className=" d-flex justify-content-between ">
+                <h5>Principal Amount</h5>
+                <p>₹{EMI_Values.principalAmount}</p>
+              </div>
+              <div className=" d-flex justify-content-between ">
+                <h5>Total Interest</h5>
+                <p>₹{EMI_Values.totalInterest}</p>
+              </div>
+              <div className=" d-flex justify-content-between ">
+                <h5>Total Amount</h5>
+                <p>₹{EMI_Values.totalAmount}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -93,7 +198,7 @@ export function App() {
   );
 }
 
-                                                  // With Calculate button
+// With Calculate button (1.0)
 // export function App() {
 //     const [EMI_Values,setEMI_Values] = useState({
 //         emi: undefined,
@@ -124,7 +229,6 @@ export function App() {
 //         let p = userInputs.loanAmount;
 //         let r = userInputs.rateOfInterest/(12*100);
 //         let n = userInputs.loanTenure*12;
-
 
 //         setEMI_Values({
 //             emi: Math.trunc( ( p*r*(1+r)**n )/( (1+r)**n - 1 ) ),
--- src/component/input/input.jsx
+++ src/component/input/input.jsx
@@ -1,83 +1,141 @@
 import { useState } from "react";
 
 // Without Calculate Button
-export function Input({ EMICalculation }) {
-  const [userInputs, setUserInputs] = useState({
-    loanAmount: 100000,
-    rateOfInterest: 1,
-    loanTenure: 1,
-  });
-  console.log("userInput - >", userInputs);
+// export function Input({ EMICalculation }) {
+//   const [userInputs, setUserInputs] = useState({
+//     loanAmount: 100000,
+//     rateOfInterest: 1,
+//     loanTenure: 1,
+//   });
+  
+//   return (
+//     <>
+//       <div className="mb-3">
+//         <div className=" d-flex justify-content-between ">
+//             <label className="form-label">Loan Amount(₹)</label>
+//             <h6>[Minimum 1Lakh]</h6>
+//         </div>
+//         <input
+//           type="text"
+//           className="form-control me-2"
+//           id="name"
+//           placeholder="Enter Amount"
+//           aria-label="Search"
+//           defaultValue={10000}
+//           onChange={(event) => {
+//             const newInputs = { ...userInputs, loanAmount: event.target.value };
+//             setUserInputs(newInputs);
+//             EMICalculation(newInputs);
+//           }}
+//         />
+//       </div>
+//       <div className="mb-3">
+//         <div className=" d-flex justify-content-between ">
+//             <label className="form-label">Rate of Interest(%)</label>
+//             <h6>[1-30]</h6>
+//         </div>
+//         <input
+//           type="text"
+//           className="form-control me-2"
+//           id="name"
+//           placeholder="Enter Percentage"
+//           aria-label="Search"
+//           defaultValue={1}
+//           onChange={(event) => {
+//             const newInputs = {
+//               ...userInputs,
+//               rateOfInterest: event.target.value
+//             }
+//             setUserInputs(newInputs);
+//             EMICalculation(newInputs);
+//           }}
+//         />
+//       </div>
+//       <div className="mb-3">
+//         <div className=" d-flex justify-content-between ">
+//             <label className="form-label">Loan Tenure(Yr)</label>
+//             <h6>[1-30]</h6>
+//         </div>
+//         <input
+//           type="text"
+//           className="form-control me-2"
+//           id="name"
+//           placeholder="Enter No of Years"
+//           aria-label="Search"
+//           defaultValue={1 }
+//           onChange={(event) => {
+//             const newInputs = {
+//               ...userInputs,
+//               loanTenure: event.target.value
+//             }
+//             setUserInputs(newInputs);
+//             EMICalculation(newInputs);
+//           }}
+//         />
+//       </div>
+//     </>
+//   );
+// }
+
+
+                                      // With Calculate button(2.0)
+export function Input({loanAmount,rateOfInterest,loanTenure,updatedInputs,EMICalculation}) {
   return (
     <>
-      <div className="mb-3">
-        <div className=" d-flex justify-content-between ">
-            <label className="form-label">Loan Amount(₹)</label>
-            <h6>[Minimum 1Lakh]</h6>
+        <div className="mb-3">
+          <div className=" d-flex justify-content-between ">
+             <label className="form-label">Loan Amount(₹)</label>
+             <h6>[Minimum 1Lakh]</h6>
+          </div>
+          <input
+            type="text"
+            className="form-control me-2"
+            id="name"
+            placeholder="Enter Amount"
+            aria-label="Search"
+            defaultValue={loanAmount}
+            onChange={ (event)=>{updatedInputs("loanAmount",event.target.value)} }
+          />
         </div>
-        <input
-          type="text"
-          className="form-control me-2"
-          id="name"
-          placeholder="Enter Amount"
-          aria-label="Search"
-          defaultValue={10000}
-          onChange={(event) => {
-            const newInputs = { ...userInputs, loanAmount: event.target.value };
-            setUserInputs(newInputs);
-            EMICalculation(newInputs);
-          }}
-        />
-      </div>
-      <div className="mb-3">
-        <div className=" d-flex justify-content-between ">
-            <label className="form-label">Rate of Interest(%)</label>
-            <h6>[1-30]</h6>
+        <div className="mb-3">
+          <div className=" d-flex justify-content-between ">
+             <label className="form-label">Rate of Interest(%)</label>
+             <h6>[1-30]</h6>
+          </div>
+          <input
+            type="text"
+            className="form-control me-2"
+            id="name"
+            placeholder="Enter Percentage"
+            aria-label="Search"
+            defaultValue={rateOfInterest}
+            onChange={ (event)=>{updatedInputs("rateOfInterest",event.target.value)} }
+          />
         </div>
-        <input
-          type="text"
-          className="form-control me-2"
-          id="name"
-          placeholder="Enter Percentage"
-          aria-label="Search"
-          defaultValue={1}
-          onChange={(event) => {
-            const newInputs = {
-              ...userInputs,
-              rateOfInterest: event.target.value
-            }
-            setUserInputs(newInputs);
-            EMICalculation(newInputs);
-          }}
-        />
-      </div>
-      <div className="mb-3">
-        <div className=" d-flex justify-content-between ">
-            <label className="form-label">Loan Tenure(Yr)</label>
-            <h6>[1-30]</h6>
+        <div className="mb-3">
+          <div className=" d-flex justify-content-between ">
+             <label className="form-label">Loan Tenure(Yr)</label>
+             <h6>[1-30]</h6>
+          </div>
+          <input
+            type="text"
+            className="form-control me-2"
+            id="name"
+            placeholder="Enter No of Years"
+            aria-label="Search"
+            defaultValue={loanTenure}
+            onChange={ (event)=>{updatedInputs("loanTenure",event.target.value)} }
+          />
         </div>
-        <input
-          type="text"
-          className="form-control me-2"
-          id="name"
-          placeholder="Enter No of Years"
-          aria-label="Search"
-          defaultValue={1 }
-          onChange={(event) => {
-            const newInputs = {
-              ...userInputs,
-              loanTenure: event.target.value
-            }
-            setUserInputs(newInputs);
-            EMICalculation(newInputs);
-          }}
-        />
-      </div>
+        <button type="button" className="btn btn-outline-primary" onClick={EMICalculation}>Calculate</button>
     </>
   );
 }
 
-// With Calculate button
+
+
+
+// With Calculate button(1.0)
 // export function Input({EMICalculation}) {
 //     const [userInputs,setUserInputs] = useState({
 //         loanAmount:undefined,
